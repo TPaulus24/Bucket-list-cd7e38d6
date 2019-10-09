43851dc3d425f4d9bfa866374a78df773701abfc
FIRST_TOM LAST_PAULUS <54597104+TPaulus24@users.noreply.github.com>
2019-10-09 09:37:51 +0200

    Uitwerking van de exercise

--- bucket_list.php
+++ bucket_list.php
@@ -1,17 +1,19 @@
 <?php
-echo 'Hoe veel activiteiten wil je toevoegen?' . PHP_EOL;
-$activiteit = readline();
-if (is_numeric($activiteit));
-elseif (!is_numeric($activiteit)) {
-    echo "$activiteit" . ' is geen getal,probeer het opnieuw.';
+echo "Hoeveel activiteiten wil je toevoegen?" . PHP_EOL;
+$act = readline();
+if (!is_numeric($act)) {
+    echo "$act" . ' is geen getal, probeer het opnieuw.';
     exit;
 }
-echo "Je gaat $activiteit activiteiten toevoegen!" . PHP_EOL;
-$actie1 = array(
+echo "Je gaat $act activiteiten toevoegen." . PHP_EOL;
+$actie = array(
+
 );
-for ($a = 1; $a <= $activiteit; $a++) {
-    array_push($action, readline("Wat wil je toevoegen aan jouw bucketlist?" . PHP_EOL));
+for ($l = 1; $l <=$act; $l++){
+    array_push($actie, readline("wat wil je toevoegen aan je bucketlist?" . PHP_EOL));
+
 }
-echo "Op je bucket list staat: " . PHP_EOL;
-foreach($actie1 as $actie2)
-echo "$actie2".PHP_EOL;
+echo "Op je bucketlist staat: " . PHP_EOL;
+foreach ($actie as $actie2) {
+    echo "$actie2" . PHP_EOL;
+}
